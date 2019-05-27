5fd21363b9e063f06e50b0e215d55eea270bff31
OsmiumDust <38886849+OsmiumDust@users.noreply.github.com>
2019-05-27 14:02:59 -0400

    Add files via upload

---
+++ Rooms.py
@@ -0,0 +1,192 @@
+import arcade
+
+sprite_scale = 0.5
+native_sprite = 128
+sprite_size = int(sprite_scale * native_sprite)
+
+screen_width = sprite_size * 18
+screen_height = sprite_size * 10
+screen_title = "Temporary Title"
+
+class Room:
+
+    def __init__(self):
+        self.wall_list = None
+
+        self.background_list = None
+
+
+def startroom_setup():
+    room = Room()
+
+    room.wall_list = arcade.SpriteList()
+
+    for y in (0, screen_height - sprite_size):
+        # Loop for each box going across
+        for x in range(0, screen_width, sprite_size):
+            if (
+                    x != sprite_size * 3 and x != sprite_size * 4 and x != sprite_size * 10 and x != sprite_size * 11) or y == 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    for x in (0, screen_width - sprite_size):
+        # Loop for each box going across
+        for y in range(sprite_size, screen_height - sprite_size, sprite_size):
+            # Skip making a block 4 and 5 blocks up on the right side
+            if (y != sprite_size * 4 and y != sprite_size * 5) or x == 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+    wall.left = 7 * sprite_size
+    wall.bottom = 5 * sprite_size
+    room.wall_list.append(wall)
+
+    room.background = arcade.load_texture("images/background_2.jpg")
+
+    return room
+
+
+def outside1_setup():
+    room = Room()
+
+    room.wall_list = arcade.SpriteList()
+
+    for y in (0, screen_height - sprite_size):
+        # Loop for each box going across
+        for x in range(0, screen_width, sprite_size):
+            if (x != sprite_size * 7 and x != sprite_size * 8) or y == 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    for x in (0, screen_width - sprite_size):
+        # Loop for each box going across
+        for y in range(sprite_size, screen_height - sprite_size, sprite_size):
+            # Skip making a block 4 and 5 blocks up
+            if (y != sprite_size * 4 and y != sprite_size * 5) or x != 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+    wall.left = 7 * sprite_size
+    wall.bottom = 5 * sprite_size
+    room.wall_list.append(wall)
+
+    room.background = arcade.load_texture("images/background_2.jpg")
+
+    return room
+
+
+def startcave_setup():
+    room = Room()
+
+    room.wall_list = arcade.SpriteList()
+
+    for x in (0, screen_width - sprite_size):
+        for y in range(0, screen_height, sprite_size):
+            wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+            wall.left = x
+            wall.bottom = y
+            room.wall_list.append(wall)
+
+    for y in (0, screen_height - sprite_size):
+        for x in range(0, screen_width, sprite_size):
+            if (x != sprite_size * 3 and x != sprite_size * 4) or y != 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+    wall.left = 7 * sprite_size
+    wall.bottom = 5 * sprite_size
+    room.wall_list.append(wall)
+
+    room.background = arcade.load_texture("images/background.jpg")
+
+    return room
+
+
+def outside2_setup():
+    room = Room()
+
+    room.wall_list = arcade.SpriteList()
+
+    for x in (0, screen_width - sprite_size):
+        for y in range(0, screen_height, sprite_size):
+            if (y != sprite_size * 4 and y != sprite_size * 5) or x != 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    for y in (0, screen_height - sprite_size):
+        for x in range(0, screen_width, sprite_size):
+            if (x != sprite_size * 7 and x != sprite_size * 8) or y != 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    room.background = arcade.load_texture("images/background_2.jpg")
+
+    return room
+
+
+def outside3_setup():
+    room = Room()
+
+    room.wall_list = arcade.SpriteList()
+
+    for x in (0, screen_width - sprite_size):
+        for y in range(0, screen_height, sprite_size):
+            if (y != sprite_size * 4 and y != sprite_size * 5):
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    for y in (0, screen_height - sprite_size):
+        for x in range(0, screen_width, sprite_size):
+            if (x != sprite_size * 10 and x != sprite_size * 11) or y != 0:
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    room.background = arcade.load_texture("images/background_2.jpg")
+
+    return room
+
+def outside4_setup():
+    room = Room()
+
+    room.wall_list = arcade.SpriteList()
+
+    for x in (0, screen_width - sprite_size):
+        for y in range(0, screen_height, sprite_size):
+            if (y != sprite_size * 4 and y != sprite_size * 5):
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    for y in (0, screen_height - sprite_size):
+        for x in range(0, screen_width, sprite_size):
+            if (x != sprite_size * 10 and x != sprite_size * 11):
+                wall = arcade.Sprite("images/boxCrate_double.png", sprite_scale)
+                wall.left = x
+                wall.bottom = y
+                room.wall_list.append(wall)
+
+    room.background = arcade.load_texture("images/background_2.jpg")
+
+    return room
